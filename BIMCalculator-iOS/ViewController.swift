1cde6eaa0d46adb6471eaacb9f4b160ed8e81e6d
Giulia0376 <giulia.13.09@hotmail.com>
2025-06-09 19:15:31 +0200

    First version

--- BIMCalculator-iOS/ViewController.swift
+++ BIMCalculator-iOS/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var weight: Float = 0
+    var height: Float = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onWeightChanged(_ sender: UIStepper) {
+        weight = Float(sender.value)
+        weightLabel.text = "\(Int (weight)) kg"
+    }
+    
+    @IBAction func onHeightChanged(_ sender: UISlider) {
+        
+        height = sender.value
+        heightLabel.text = "\(Int(height)) cm"
 
+    }
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        let heightInMeter:Float = height / Float (100)
+        let bmi: Float = weight / pow(heightInMeter, 2)
+        resultLabel.text = "\(String (format: "%.2f", bmi))"
+        
+    }
+    
 }
 
